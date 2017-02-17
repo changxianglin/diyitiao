b192358e3082a7951e8db95a8ed947b74a1cf4ea
xianglin zhang <281532612@qq.com>
2017-02-17 22:46:03 +0800

    2
    demo

---
+++ 2/example.js
@@ -0,0 +1,151 @@
+// mood = "happy";
+// age = 33;
+// alert(mood);
+// alert(age);
+// var height = "about 5'10\" tall"
+// alert(height);
+// var beatles = Array(4);
+// beatles[0] = "John";
+// beatles[1] = "Paul";
+// beatles[2] = "George";
+// beatles[3] = "Ringo";
+// alert(beatles);
+// var beatles = Array("John" , "Paul" , "George" , "Ringo");
+// alert(beatles[2]);
+// var beatles = ["John" , "Paul" , "George" , "Ringo"]
+// var Years = [1940 , 1941 , 1942, 1943];
+// var lennon = ["John" , "1940" , false]
+// var name = "John";
+// beatles[0] = name;
+// var lennon = ["John" , "1940" , false]
+// var beatles = [];
+// beatles = lennon;
+// alert(beatles);
+// var lennon = Array();
+// lennon["name"] = "John";
+// lennon["year"] = "1940";
+// lennon["living"] = false;
+// var lennon = Object();
+// lennon.name = "John";
+// lennon.year = "1940";
+// lennon.living = false;
+// var lennon = {name:"John" , year:1940 , living:false};
+// var beatles = Array();
+// beatles[0] = lennon;
+// alert(beatles[0].name);
+// var beatles = {};
+// beatles.vocalist = lennon;
+// alert(beatles.vocalist.name);
+// alert(beatles.vocalist.year);
+// alert(beatles.vocalist.living);
+// var sum = 1 + 4 * 5;
+// var total = (1 + 4) * 5;
+// var temp_fahrenheit = 95;
+// var temp_celsius = (temp_fahrenheit - 32) / 1.8;
+// year = year + 1;
+// year++;
+// var message = "I am feeling" + "happy";
+// var mood = "happy";
+// var message = "I am feeling" + mood;
+// var year = 2005;
+// var message = "The year is" + year;
+// alert("10" + "20");
+// alert(10 + 20);
+// var year = 2010;
+// var message = "The year is ";
+// message += year;
+// alert(message);
+// if(1 > 2) {
+//   alert("The world has gone mad!");
+// }
+// if(1 > 2) {
+//   alert("The world is gone mad!");
+// } else {
+//   alert("All is well with the world");
+// }
+// var my_mood = "happy";
+// var your_mood = "sad";
+// if (my_mood == your_mood) {
+//   alert("We both feel the same.");
+// }
+//
+// if (my_mood != your_mood) {
+//   alert("We're feeling different moods.");
+// }
+// var a = false;
+// var b = "";
+// if(a === b) {
+//   alert("a equals b");
+// }
+// if( num >= 5 && num <= 10) {
+//   alert("The number is in the right range.");
+// }
+// if(num > 10 || num < 5) {
+//   alert("The number is not in the right range.")
+// }
+// if(!(1 > 2)) {
+//   alert("All is well with the world");
+// }
+//
+// if ( !(num > 10 || num < 5)){
+//   alert("The number is in the right range");
+// }
+// var count = 1;
+// while (count < 11) {
+//   alert(count);
+//   count++;
+// }
+// var count = 1;
+// do {
+//   alert(count);
+//   count++;
+// } while (count < 11);
+// var count = 1;
+// do {
+//   alert(count);
+//   count++;
+// } while (count < 1);
+// for (var count = 1; count < 11; count++) {
+//   alert(count);
+// }
+// var beatles = Array("John" , "Paul" , "George" , "Ringo");
+// for( var count = 0; count < beatles.length;count++) {
+//   alert(beatles[count]);
+// }
+// function shout() {
+//   var beatles = Array("John" , "Paul" , "George" , "Ringo");
+//   for(var count = 0 ; count < beatles.length ; count++) {
+//     alert(beatles[count]);
+//   }
+// }
+// shout();
+// function multiply(num1, num2) {
+//   var total = num1 * num2;
+//   alert(total);
+// }
+// multiply(10 , 2)
+// function converToCelsius(temp) {
+//
+//   var result = temp - 32;
+//   result = result / 1.8;
+//   return result;
+// }
+//
+// var temp_fahrenheit = 95;
+// var temp_celsius = converToCelsius(temp_fahrenheit);
+// alert(temp_celsius);
+// function square(num) {
+//
+//   var total = num * num;
+//   return total;
+// }
+// var total = 50;
+// var number = square(20);
+// alert(total)
+// var jeremy = new Person;
+// jeremy.age;
+// jeremy.mood;
+// var beatles = new Array();
+var num = 7.561;
+var num = Math.round(num);
+alert(num);

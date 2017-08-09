2e59c36cef5c248cc81d954be082ad7db4034196
changxianglin <281532612@qq.com>
2017-08-09 19:44:01 +0800

    scripit

---
+++ mode.js
@@ -0,0 +1,90 @@
+var er = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+var insertNum = function(a) {
+    var j = []
+for (var i = 0; i < a.length; i++) {
+    if (i == a.length / 2) {
+     j.push(100)
+    }
+    j.push(a[i])
+}
+return j
+}
+insertNum(er)
+
+
+var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+var re = function(b) {
+var c = []
+for (var i = 0; i < b.length; i++) {
+    c.push(b[i])
+}
+    return c
+}
+re(num)
+
+
+var yesNum = function(numb) {
+var j = []
+for (var i = 0; i < numb ; i++) {
+    j.push(i)
+}
+return j
+}
+yesNum(200)
+
+
+var clearnum = function(num) {
+    var k = []
+    var a = num[0]
+    for (var i = 0; i < num.length; i++) {
+        if (a == )
+    }
+}
+
+Array.prototype.push = function(num) {
+    this[this.length] = num
+}
+
+
+var values = [213, 16, 2058, 54, 10, 57, 9]
+values.sort()
+
+function outer() {
+    var a = 1
+
+    function inner() {
+
+    }
+
+    var b = 2
+
+    if (a === 1) {
+    var c = 3
+    }
+}
+outer()
+
+function creep() {return this}
+new creep()
+
+window.onload = function() {assert(true, "power!")}
+
+var ninja = {
+    shout: function() {
+        assert(true, "Niaja")
+    }
+}
+niaja.shout()
+
+setTimeout(
+    function() {assert(true, "Forever")}
+)
+function bottMeUp() {assert(true, "power!")}
+window.onload = bottMeUp;
+
+var obj = {}
+
+var fn = function() {}
+
+assert(obj && fn, "Both the object and function exist.")

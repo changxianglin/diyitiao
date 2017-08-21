35e110ca72c3735e8e343a3dbb2cf4705da9a644
changxianglin <281532612@qq.com>
2017-08-21 17:08:16 +0800

    引用类型

--- study/demo01.js
+++ study/demo01.js
@@ -291,6 +291,181 @@ var demo02 = function() {
     }
     log("2017.08.21")
     //
+    // String 类型
+    var stringObject = new String("Hello world")
+
+    var StringValue = "hello world"
+    log(StringValue.length)
+
+    var stringValue = "hello world"
+    log("字符", stringValue.charAt(1))
+    log("编码", stringValue.charCodeAt(1))
+
+    var stringValue = "hello world"
+    log(stringValue[1])
+
+    var stringValue = "hello "
+    var result = stringValue.concat("world")
+    log(result)
+    log(stringValue)
+
+    var stringValue = "hello "
+    var result = stringValue.concat("world", " !")
+    log(result)
+    log(stringValue)
+
+
+    var stringValue = "hello world"
+    log(stringValue.slice(3))
+    log(stringValue.substring(3))
+    log(stringValue.substr(3))
+    log(stringValue.slice(3, 7))
+    log(stringValue.substring(3, 7))
+    log(stringValue.substr(3, 7))
+
+    var stringValue = "hello world"
+    log(stringValue.slice(-3))
+    log(stringValue.substring(-3))
+    log(stringValue.substr(-3))
+    log(stringValue.slice(3, -4))
+    log(stringValue.substring(3, -4))
+    log(stringValue.substr(3, 7))
+
+    var stringValue = "hello world"
+    log("indexOf", stringValue.indexOf("o"))
+    log(stringValue.lastIndexOf("o"))
+
+    var stringValue = "hello world"
+    log(stringValue.indexOf("o", 6))
+    log(stringValue.lastIndexOf("o", 6))
+
+    var stringValue = "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+    var positions = new Array()
+    var pos = stringValue.indexOf("e")
+    while (pos > -1) {
+        positions.push(pos)
+        pos = stringValue.indexOf("e", pos + 1)
+    }
+    log(positions)
+
+    var stringValue = "  hello world   "
+    var trimmeStringValue = stringValue.trim()
+    log(stringValue)
+    log(trimmeStringValue)
+
+    // 大小写转换
+    var stringValue = "hello world"
+    log(stringValue.toLocaleUpperCase())
+    log(stringValue.toUpperCase())
+    log(stringValue.toLocaleLowerCase())
+    log(stringValue.toLowerCase())
+
+    // 字符串匹配方法
+    var text = "cat, bat, sat, fat"
+    var pos = text.search(/at/)
+    log(pos)
+
+    var result = text.replace("at", "ond")
+    log(result)
+
+    var result = text.replace(/at/g, "ond")
+    log(result)
+
+    var colorText = "red, blue, green, yellow"
+    var colors1 = colorText.split(",")
+    var colors2 = colorText.split(",", 2)
+    log(colors1)
+    log(colors2)
+
+    var stringValue = "yellow"
+    log(stringValue.localeCompare("brick"))
+    log(stringValue.localeCompare("yellow"))
+    log(stringValue.localeCompare("zoo"))
+
+    function determineOrder(value) {
+        var result = stringValue.localeCompare(value)
+        if (result < 0) {
+            log("The string 'yellow' comes before the string ")
+        } else if (result > 0) {
+            log("The string 'yellow' come after")
+        } else {
+            log(("The string 'yellow' is equal to the string"))
+        }
+    }
+    determineOrder("brick")
+    determineOrder("yellow")
+    determineOrder("zoo")
+
+    log(String.fromCharCode(104, 101, 108, 108, 111))
+
+    log("global")
+    var uri = "http://www.wrox.com/illegal value.htm#start"
+    log(encodeURI(uri))
+    log(encodeURIComponent(uri))
+
+    // eval strong
+    eval("log('hi')")
+
+    var msg = "hello world"
+    eval("log(msg)")
+
+    eval("function sayHi() { log('HI')}")
+    sayHi()
+
+    eval("var msg = 'hello world'")
+    log(msg)
+
+    // var color = "red"
+    // function sayColor() {
+    //     log(window.color)
+    // }
+    // window.sayColor()
+
+    var max = Math.max(3, 54, 32, 16)
+    log(max)
+
+    var min = Math.min(3, 54, 32, 16)
+    log(min)
+
+    var values = [1, 2, 3, 4, 5, 6, 7, 8]
+    var max = Math.max.apply(Math, values)
+    log(max)
+
+    log("ceil")
+    log(Math.ceil(25.9))
+    log(Math.ceil(25.4))
+    log(Math.ceil(25.1))
+
+    log("round")
+    log(Math.round(25.9))
+    log(Math.round(25.4))
+    log(Math.round(25.1))
+
+    log("floor")
+    log(Math.floor(25.9))
+    log(Math.floor(25.4))
+    log(Math.floor(25.1))
+
+    log("random")
+
+    // 值 = Math.floor(Math.random() * 可能值得总数 + 第一个可能的值)
+    var numm = Math.floor(Math.random() * 10 + 1)
+    log(numm)
+
+    var num = Math.floor(Math.random() * 9 + 2)
+    log(num)
+
+    function selecFrom(lowerValue, upperValue) {
+        var choices = upperValue - lowerValue + 1
+        return Math.floor(Math.random() * choices + lowerValue)
+    }
+
+    var num = selecFrom(2, 10)
+    log(num)
+
+    var colors = [ "red", "green", "blue", "yellow", "black", "purple", "brown"]
+    var color = colors[selecFrom(0, colors.length - 1)]
+    log(color)
 
 }
 demo02()

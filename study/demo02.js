e239c7706701388a7e6beff0c9be967d996577be
changxianglin <281532612@qq.com>
2017-08-22 23:57:27 +0800

    object

--- study/demo02.js
+++ study/demo02.js
@@ -64,5 +64,79 @@ log(book.edition)
 
 // 创建对象
 
+// 工厂模式
+function createPerson(name, age, job) {
+    var o = new Object()
+    o.name = name
+    o.age = age
+    o.sayName = function() {
+        log(this.name)
+    }
+    return o
+}
+
+var person1 = createPerson("Nicholas", 29, "Software Engineer")
+var person2 = createPerson("Greg", 27, "Doctor")
+log(person1.name)
+log(person2.name)
+
+// 构造函数模式
+function Person(name, age, job) {
+    this.name = name
+    this.age = age
+    this.job = job
+    this.sayName = function() {
+        log(this.name)
+    }
+}
+
+var person1 = new Person("Nicholas", 29, "Software Engineer")
+var person2 = new Person("Greg", 27, "Doctor")
+
+log(person1.name)
+log(person2.name)
+
+log(person1.constructor == Person)
+log(person2.constructor == Person)
+
+log(person1 instanceof Object)
+log(person1 instanceof Person)
+log(person2 instanceof Object)
+log(person2 instanceof Person)
+
+
+// 构造函数当作函数
+var person = new Person("Nicholas", 29, "Software Engineer")
+person.sayName()
+
+Person("Greg", 27, "Doctor")
+// window.sayName()
+
+var o = new Object()
+Person.call(o, "Kristen", 25, "Nurse")
+// o.sayName()
+
+// 构造函数的问题
+// function Person(name, age, job) {
+//     this.name = name
+//     this.age = age
+//     this.job = job
+//     this.sayName = new Function("log(this.name)")
+// }
+// log(person1.sayName == perons2.sayName)
+function Person0(name, age, job) {
+    this.name = name
+    this.age = age
+    this.job = job
+    this.sayName = sayName
+}
+
+function sayName() {
+    log(this.name)
+}
+
+var person1 = new Person0("Nichoals", 29, "Software Engineer")
+var person2 = new Person0("Greg", 27, "Doctor")
+
 }
 demo03()
